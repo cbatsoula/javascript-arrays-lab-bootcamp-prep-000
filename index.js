424c45cb0a70f8366a1701bf2f82b2ba997ed9c4
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-03 03:50:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-var moreKittens = [""]
-var evenMoreKittens = [""]
+var moreKittens = ["Broom"]
+var evenMoreKittens = ["Arnold"]
 
 function destructivelyAppendKitten(array, name){
   return kittens.push("Ralph");
